780be28703635792b96d1fdc88918cfa2f1827f6
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-10 22:05:57 -0500

    MODO IA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reportes-tga-v4';
+const CACHE = 'reportes-tga-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
