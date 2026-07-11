22f1447eabbd455eb3bfff3064adc5e9988c883e
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-10 22:32:49 -0500

    Nueva version con IA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reportes-tga-v7';
+const CACHE = 'reportes-tga-v8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
