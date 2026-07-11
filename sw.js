1e83450872291c5b57fb7637eede4f3c869b5abb
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-10 22:13:54 -0500

    Update cache version to 'reportes-tga-v6'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'reportes-tga-v5';
+const CACHE = 'reportes-tga-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
