55e81b30ee686eaf91e52ebd481f7e0cca6ed695
rolisinr <rolisilvestrerojas@gmail.com>
2026-07-12 06:27:51 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-const CACHE = 'cov-reportes-v1';
+const CACHE = 'cov-reportes-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // Firebase init en SW
