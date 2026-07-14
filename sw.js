358336c00e6ee0a3301962e40cfcaeea26f4c9b4
COV Reportes <roli@covapp.online>
2026-07-14 03:02:25 +0000

    Fix mensajes QAP/SSHH: sin ubicación, prefijos correctos (v32)

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-const CACHE = 'cov-reportes-v31';
+const CACHE = 'cov-reportes-v32';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 // Firebase init en SW
